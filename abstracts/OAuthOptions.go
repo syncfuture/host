5a5d49c279d3191af67142394e0c1ee67bb93c19
Lukiya <lukiya@outlook.com>
2021-03-16 13:51:04 -0700

    update

--- abstracts/OAuthOptions.go
+++ abstracts/OAuthOptions.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Lukiya/oauth2go"
 	log "github.com/syncfuture/go/slog"
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
 )
 
 type (
@@ -31,5 +32,20 @@ func (x *OAuthOptions) BuildOAuthOptions() {
 	}
 	if x.EndSessionEndpoint == "" {
 		log.Fatal("OAuth.EndSessionEndpoint cannot be empty")
+	}
+	// if x.ClientID == "" {
+	// 	log.Fatal("OAuth.ClientID cannot be empty")
+	// }
+	// if x.ClientSecret == "" {
+	// 	log.Fatal("OAuth.ClientSecret cannot be empty")
+	// }
+
+	x.ClientCredential = &oauth2go.ClientCredential{
+		Config: &clientcredentials.Config{
+			ClientID:     x.ClientID,
+			ClientSecret: x.ClientSecret,
+			TokenURL:     x.Endpoint.TokenURL,
+			Scopes:       x.Scopes,
+		},
 	}
 }
--- publish.ps1
+++ publish.ps1
@@ -1,7 +1,9 @@
+$OldTag = v1.3.15
+$NewTag = v1.3.16
 #git tag | foreach-object -process { git push origin --delete $_ }
 #git tag | foreach-object -process { git tag -d $_ }
-git push origin --delete v1.3.14
-git tag -d v1.3.14
-git tag v1.3.15
+git push origin --delete $OldTag
+git tag -d $OldTag
+git tag $NewTag
 git push
 git push --tags
