99a37eab1190483d4daa4d96013c716e1748260e
Lukiya <lukiya@outlook.com>
2021-03-18 17:39:25 -0700

    update

--- client/interfaces.go
+++ client/interfaces.go
@@ -1,9 +1,14 @@
 package client
 
-import "github.com/syncfuture/host"
+import (
+	"github.com/syncfuture/host"
+	"golang.org/x/oauth2"
+)
 
 type IOAuthClientHost interface {
 	host.IBaseHost
 	host.IWebHost
 	AuthHandler(ctx host.IHttpContext)
+	GetClientToken(ctx host.IHttpContext) (*oauth2.Token, error)
+	GetUserToken(ctx host.IHttpContext) (*oauth2.TokenSource, error)
 }
--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,5 @@
-$OldTag = "v1.3.35"
-$NewTag = "v1.3.36"
+$OldTag = "v1.3.36"
+$NewTag = "v1.3.37"
 #git tag | foreach-object -process { git push origin --delete $_ }
 #git tag | foreach-object -process { git tag -d $_ }
 git push origin --delete $OldTag
