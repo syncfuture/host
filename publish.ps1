0567665e76453d1a41c54c5e17321989a0430c32
Lukiya <lukiya@outlook.com>
2021-03-23 10:09:30 -0700

    update

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,5 @@
-$OldTag = "v1.3.48"
-$NewTag = "v1.3.49"
+$OldTag = "v1.3.49"
+$NewTag = "v1.3.50"
 #git tag | foreach-object -process { git push origin --delete $_ }
 #git tag | foreach-object -process { git tag -d $_ }
 git push origin --delete $OldTag
