5ec0a80c3d82329a28c35c482fdea5eb516f310e
Lukiya <lukiya@outlook.com>
2021-11-17 10:33:37 -0800

    u

--- sconsul/sconsul.go
+++ sconsul/sconsul.go
@@ -17,6 +17,7 @@ func RegisterServiceInfo(cp sconfig.IConfigProvider, host service.IServiceHost) 
 	serviceCheckInterval := cp.GetString("Consul.Service.Check.Interval")
 	serviceHost := cp.GetString("Consul.Service.Host")
 	servicePort := cp.GetInt("Consul.Service.Port")
+	serviceID := fmt.Sprintf("%v[%v:%v]", serviceName, serviceHost, servicePort)
 
 	// 服务中心客户端
 	consulConfig := api.DefaultConfig()
@@ -27,14 +28,14 @@ func RegisterServiceInfo(cp sconfig.IConfigProvider, host service.IServiceHost) 
 
 	// 在服务中心登记服务
 	err = consulAgent.ServiceRegister(&api.AgentServiceRegistration{
-		ID:   fmt.Sprintf("%v[%v:%v]", serviceName, host.GetHost(), host.GetPort()), // 服务节点的名称
-		Name: serviceName,                                                           // 服务名称
+		ID:   serviceID,   // 服务节点的名称
+		Name: serviceName, // 服务名称
 		// Tags:    r.Tag,                                        // tag，可以为空
 		Address: serviceHost, // 服务 IP
 		Port:    servicePort, // 服务端口
 		Check: &api.AgentServiceCheck{ // 健康检查
 			Interval:                       serviceCheckInterval, // 健康检查间隔
-			TCP:                            host.GetListenAddr(),
+			TCP:                            fmt.Sprintf("%s:%d", serviceHost, servicePort),
 			DeregisterCriticalServiceAfter: serviceCheckTimeout, // 注销时间，相当于过期时间
 		},
 	})
