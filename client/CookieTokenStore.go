8d7aabb06687b3a261bc3c45ac47300e06b9ae7a
Lukiya Chen <lukiya@outlook.com>
2025-02-06 09:51:28 -0800

    u

--- client/CookieTokenStore.go
+++ client/CookieTokenStore.go
@@ -3,13 +3,9 @@ package client
 import (
 	"encoding/json"
 	"net/http"
-	"time"
 
-	oauth2core "github.com/Lukiya/oauth2go/core"
-	"github.com/pascaldekloe/jwt"
 	"github.com/syncfuture/go/serr"
 	"github.com/syncfuture/go/ssecurity"
-	"github.com/syncfuture/go/u"
 	"github.com/syncfuture/host"
 	"golang.org/x/oauth2"
 )
@@ -42,19 +38,19 @@ func (x *CookieTokenStore) SaveToken(ctx host.IHttpContext, token *oauth2.Token)
 	}
 
 	// 保存加密后的令牌到Cookie
-	tokenCookie := new(http.Cookie)
-	tokenCookie.Name = x.TokenCookieName
-	tokenCookie.Value = securedString
-	tokenCookie.HttpOnly = true
-	tokenClaims, err := jwt.ParseWithoutCheck(u.StrToBytes(token.AccessToken))
-	if err != nil {
-		return serr.WithStack(err)
-	}
-	if rexp, ok := tokenClaims.Set[oauth2core.Claim_RefreshTokenExpire].(float64); ok {
-		// claims里有刷新令牌过期时间，作为Cookie
-		tokenCookie.Expires = time.Unix(int64(rexp), 0)
-		// 否则作为session存储
-	}
+	// tokenCookie := new(http.Cookie)
+	// tokenCookie.Name = x.TokenCookieName
+	// tokenCookie.Value = securedString
+	// tokenCookie.HttpOnly = true
+	// tokenClaims, err := jwt.ParseWithoutCheck(u.StrToBytes(token.AccessToken))
+	// if err != nil {
+	// 	return serr.WithStack(err)
+	// }
+	// if rexp, ok := tokenClaims.Set[oauth2core.Claim_RefreshTokenExpire].(float64); ok {
+	// 	// claims里有刷新令牌过期时间，作为Cookie
+	// 	// tokenCookie.Expires = time.Unix(int64(rexp), 0)
+	// 	// 否则作为session存储
+	// }
 
 	ctx.SetCookieKV(x.TokenCookieName, securedString, func(c *http.Cookie) {
 		c.HttpOnly = true
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.23.4
 
 require (
 	github.com/Lukiya/logs v1.3.0
-	github.com/Lukiya/oauth2go v1.17.2
+	github.com/Lukiya/oauth2go v1.18.0
 	github.com/fasthttp/router v1.5.4
 	github.com/fasthttp/session/v2 v2.5.9
 	github.com/go-playground/form v3.1.4+incompatible
@@ -19,7 +19,7 @@ require (
 	github.com/pascaldekloe/jwt v1.12.0
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.10.0
-	github.com/syncfuture/go v1.17.2
+	github.com/syncfuture/go v1.18.0
 	github.com/valyala/fasthttp v1.58.0
 	golang.org/x/oauth2 v0.26.0
 	google.golang.org/grpc v1.70.0
@@ -70,7 +70,7 @@ require (
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250204164813-702378808489 // indirect
-	google.golang.org/protobuf v1.36.4 // indirect
+	google.golang.org/protobuf v1.36.5 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
